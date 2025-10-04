46c1ba0c9e8a47bcf4e13a2741ca0e1fd507b2e3
nuc@short-circut <officers@roboclub.org>
2025-10-04 09:42:21 -0400

    hotfix (stampedpose)

--- rb_ws/src/buggy/scripts/estimation/ukf_node.py
+++ rb_ws/src/buggy/scripts/estimation/ukf_node.py
@@ -6,6 +6,7 @@ from rclpy.node import Node
 
 from std_msgs.msg import Float64, Bool
 from nav_msgs.msg import Odometry
+from buggy.msg import StampedFloat64Msg
 
 
 from ukf import *
@@ -23,7 +24,7 @@ class UKF(Node):
         self.Q = np.diag([1e-4, 1e-4, 1e-2, 2.4e-1])
 
         self.create_subscription(Odometry, "other/stateNoUKF", self.update, 1)
-        self.create_subscription(Float64, "other/steering", self.updateSteering, 1)
+        self.create_subscription(StampedFloat64, "other/steering", self.updateSteering, 1)
         self.nand_publisher = self.create_publisher(Odometry, "other/state", 10)
         self.singular_flag_publisher = self.create_publisher(Bool, "debug/NANDSingularFlag", 10)
 
